ce8e536c4460c8ea0a1fc0758488b3db92b4532c
jossdz <carloscorrea.dev@gmail.com>
2020-12-31 13:47:57 -0600

    finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,19 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import "../components/layout.css"
 
 const IndexPage = () => (
-  <Layout>
+  <div>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+    <div className="p1"></div>
+    <div className="p2"></div>
+    <div className="p3"></div>
+    <div className="p4"></div>
+    <div className="p5"></div>
+    <div className="bp"></div>
+    <div className="bp"></div>
+    <div className="bp"></div>
+  </div>
 )
 
 export default IndexPage
